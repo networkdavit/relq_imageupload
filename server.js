a501514052392e47675e0a16b96a33cd29bd26dd
networkdavit <lonecreation1612@gmail.com>
2022-10-07 12:01:18 +0400

    Quality changes

--- server.js
+++ server.js
@@ -1,36 +1,33 @@
 const express = require('express');
 const multer = require('multer');
+const fs = require('fs');
+const app = express();
+const PORT = 3000;
 
-let id = 0
+
+app.use(express.static('public'));
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, './uploads/images')
+      cb(null, './images')
     },
     filename: function (req, file, cb) {
-      cb(null, file.fieldname + id   )
+      cb(null, file.originalname)
     }
   })
   
 const upload = multer({ storage: storage })
-const fs = require('fs');
-
-
-const app = express();
-const PORT = 3000;
-
-app.use(express.static('public'));
 
 app.post('/upload', upload.single('photo'), (req, res) => {
-    id ++
     if(req.file) {
         res.json(req.file);
     }
     else throw 'error';
 });
 
-app.get('/images/:id',(req,res)=>{
-    const image_id = req.params.id
-    fs.readFile(`./uploads/images/photo${image_id}`, function(err, data) {
+app.get('/images/:name',(req,res)=>{
+    const image_name = req.params.name
+    fs.readFile(`./images/${image_name}`, function(err, data) {
         if (err) throw err;
           res.writeHead(200, {'Content-Type': 'image/jpeg'});
           res.end(data); 

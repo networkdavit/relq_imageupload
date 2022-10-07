f027ddbfc831b4f137223c7cc5f5f7e6d96ec1ec
networkdavit <lonecreation1612@gmail.com>
2022-10-07 11:18:32 +0400

    first commit

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ To install necessary packages and start the server:
 Then go to [http://localhost:3000](http://localhost:3000). 
 
 Explanation - https://medium.com/@nitinpatel_20236/image-upload-via-nodejs-server-3fe7d3faa642
+# relq_express_image_tutorial
--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const multer = require('multer');
-const upload = multer({dest: __dirname + '/uploads/images'});
+
+let id = 0
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads/images')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + id   )
+    }
+  })
+  
+const upload = multer({ storage: storage })
+const fs = require('fs');
+
 
 const app = express();
 const PORT = 3000;
@@ -8,11 +21,21 @@ const PORT = 3000;
 app.use(express.static('public'));
 
 app.post('/upload', upload.single('photo'), (req, res) => {
+    id ++
     if(req.file) {
         res.json(req.file);
     }
     else throw 'error';
 });
+
+app.get('/images/:id',(req,res)=>{
+    const image_id = req.params.id
+    fs.readFile(`./uploads/images/photo${image_id}`, function(err, data) {
+        if (err) throw err;
+          res.writeHead(200, {'Content-Type': 'image/jpeg'});
+          res.end(data); 
+      });
+})
 
 app.listen(PORT, () => {
     console.log('Listening at ' + PORT );
